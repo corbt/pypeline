6b230ac0bcfdbc96727abe655aecbbaf380a9644
Kyle Corbitt <kyle@corbt.com>
2013-12-13 16:48:52 -0700

    basic functionality and tests

--- pypeline/DB.py
+++ pypeline/DB.py
@@ -1,6 +1,78 @@
+import json
 import plyvel
 
 class DB:
-    def __init__(self):
-        print "yay"
-    
+    def __init__(self, database_path, create_if_missing=False):
+        self.db=plyvel.DB(database_path, 
+            create_if_missing=create_if_missing)
+
+        self.collections_set=self.db.prefixed_db(b'collections/')
+        self.collection_items_set=self.db.prefixed_db(b'collection-items/')
+
+    def collection(self, collection_name):
+        data = self.collections_set.get(collection_name)
+        if data == None:
+            self.collections_set.put(collection_name, 'true')
+
+        return Collection(self.collection_items_set, collection_name)
+
+    def collections(self):
+        collections = []
+        for name, value in self.collections_set:
+            collections.append(Collection(self.collection_items_set, name))
+
+        return collections
+
+    def close(self):
+        self.db.close()
+
+    def open(self):
+        self.db.open()
+
+class Collection:
+    def __init__(self, items_set, name):
+        if '!!' in name:
+            raise ValueError("Disallowed character sequence '!!' in collection name")
+
+        self.name = name
+        self.db = items_set.prefixed_db(name+b'!!')
+
+        self.length = 0
+        last_instance = '0'
+        for key in self.db.iterator(include_value=False):
+            length += 1
+            last_instance = key
+            print last_instance
+
+        self.last_index = 0
+
+    def append(self, record):
+        self.db.put(str(self.last_index), json.dumps(record))
+        self.last_index += 1
+        if self.length:
+            self.length += 1
+
+    def __iter__(self):
+        return Iterator(self.db.iterator(include_key=False))
+
+    def __getitem__(self, key):
+        pass
+
+    def __setitem__(self, key):
+        pass
+
+    def __repr__(self):
+        return "%s(%r)" % (self.__class__, self.name)
+
+    def __len__(self):
+        return self.length
+
+class Iterator:
+    def __init__(self, base_iterator):
+        self.base_iterator = base_iterator
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return json.loads(self.base_iterator.next())
--- test/test_pypeline.py
+++ test/test_pypeline.py
@@ -1,3 +1,86 @@
+import os
+import tempfile
+import shutil
+
 import pytest
 
-from pypeline import DB
+from pypeline import DB, Collection
+
+@pytest.fixture
+def db_dir(request):
+    path = tempfile.mkdtemp()
+
+    def finalize():
+        shutil.rmtree(path)
+
+    request.addfinalizer(finalize)
+    return path
+
+@pytest.fixture
+def db(request):
+    path = tempfile.mkdtemp()
+    db = DB(path, create_if_missing=True)
+
+    def finalize():
+        db.close()
+        shutil.rmtree(path)
+
+    request.addfinalizer(finalize)
+    return db
+
+@pytest.fixture
+def collection(request):
+    path = tempfile.mkdtemp()
+    db = DB(path, create_if_missing=True)
+
+    collection = db.collection('test')
+
+    def finalize():
+        db.close()
+        shutil.rmtree(path)
+
+    request.addfinalizer(finalize)
+    return collection
+
+
+def test_db_creation(db_dir):
+    test_db = DB(db_dir, create_if_missing=True)
+    assert os.path.isdir(db_dir) == True
+    assert isinstance(test_db, DB) == True
+
+def test_collection_creation(db):
+    collection = db.collection(b'test')
+    assert isinstance(collection, Collection) == True
+
+    # Collection names cannot contain the reserved '!!' sequence
+    with pytest.raises(ValueError):
+        db.collection(b'a !! collection') 
+
+def test_list_collections(db):
+    collections = [b'test', b'test1', b'test2']
+    for collection in collections:
+        db.collection(collection)
+
+    loaded_collections = db.collections()
+    assert str(loaded_collections) == "[pypeline.DB.Collection('test'), pypeline.DB.Collection('test1'), pypeline.DB.Collection('test2')]"
+    assert [collection.name for collection in loaded_collections] == collections
+
+def test_collection_put(collection):
+    collection.append([1,2])
+    collection.append({'a': 'b'})
+
+    assert [c for c in collection] == [[1,2], {'a': 'b'}]
+#     assert collection[0] == {'a': 'b'}
+
+def test_collection_random_access(collection):
+    pass
+
+# def test_collection_reloading(db):
+#     pass
+
+# def test_collection_length(db):
+#     collection = db.collection(b'test')
+#     collection.append([1,2,3])
+#     collection.append([4,5,6])
+
+    # assert len(collection) == 2
