ebbbfda8beb350f28857a7c668f2431f7a89d94d
Kyle Corbitt <kyle@corbt.com>
2013-12-14 18:55:20 -0700

    mock dependency for doc building

--- doc/conf.py
+++ doc/conf.py
@@ -261,3 +261,25 @@ texinfo_documents = [
 #texinfo_no_detailmenu = False
 
 autodoc_member_order = 'bysource'
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['plyvel']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
